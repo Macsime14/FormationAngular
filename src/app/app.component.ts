33016d6330788d1ab350c01643bf3c855203c025
14A_formation B14A 755 <14A_formation.B14A@si.cnaf.info>
2019-11-04 15:08:45 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+// Déclaration en tant que WebComponent
 @Component({
+  // Appeler dans index.html
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'formation-angular';
+  public title = 'formation-angular';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UiModule } from './ui/ui.module';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
+  // Imports au démarrage de la page
   imports: [
-    BrowserModule
+    BrowserModule,
+    UiModule,
+    LoginModule
   ],
+  // Déclaration des services.
   providers: [],
+  // Démarrage du composant.
   bootstrap: [AppComponent]
 })
 export class AppModule { }
